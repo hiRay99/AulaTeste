b9abda286f307f61fa8f90004b3aa9f7cfb08b49
Rayane Ribeiro <rayaneribeiro99@hotmail.com>
2017-10-25 01:07:10 -0300

    teste 1

---
+++ WebAppPaginaMestre/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(WebAppPaginaMestre.Startup))]
+namespace WebAppPaginaMestre
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
