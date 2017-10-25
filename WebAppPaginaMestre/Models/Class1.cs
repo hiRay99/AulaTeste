0a10a8194b3917163076aea3213606b0306fcdca
Rayane Ribeiro <rayaneribeiro99@hotmail.com>
2017-10-25 02:15:47 -0300

    asec

---
+++ WebAppPaginaMestre/Models/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebAppPaginaMestre.Models
+{
+    public class Class1
+    {
+        public String Nome { get; set; }
+    }
+}
